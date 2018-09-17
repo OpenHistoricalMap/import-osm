0a0496974847bee3b2f09abd6458826752406667
Greg Allensworth <gregallensworth@users.noreply.github.com>
2018-09-17 13:15:01 -0700

    Correct use of ohm_planet PBF + Always force DIFF_MODE

--- import_osm.sh
+++ import_osm.sh
@@ -9,13 +9,8 @@ readonly DIFF_MODE=${DIFF_MODE:-true}
 
 function import_pbf() {
     local pbf_file="$1"
-    local diff_flag=""
-    if [ "$DIFF_MODE" = true ]; then
-        diff_flag="-diff"
-        echo "Importing in diff mode"
-    else
-        echo "Importing in normal mode"
-    fi
+    local diff_flag="-diff"
+    echo "Importing in diff mode"
 
     imposm3 import \
         -connection "$PG_CONNECT" \
@@ -31,7 +26,7 @@ function import_pbf() {
 function import_osm_with_first_pbf() {
     if [ "$(ls -A $IMPORT_DIR/*.pbf 2> /dev/null)" ]; then
         local pbf_file
-        for pbf_file in "$IMPORT_DIR"/*.pbf; do
+        for pbf_file in "$IMPORT_DIR"/ohm_planet_*.pbf; do
 			import_pbf "$pbf_file"
             break
         done
